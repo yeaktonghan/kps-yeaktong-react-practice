e51992be3a68f83550687deff910232d01bdd058
yeaktonghan <yeaktonghan.github@gmail.com>
2023-02-23 09:59:23 +0700

    add tailwind

---
+++ src/component/Hello.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div class="text-center text-4xl text-red-800">Hello</div>
+    )
+  }
+}
